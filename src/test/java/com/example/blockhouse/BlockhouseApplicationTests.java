7c642f3c0156e352dddc2b5c66fd2927ce420f20
Catalin Lazar <catalin.lazar@endava.com>
2018-05-23 14:45:15 +0300

    Initial commit

---
+++ src/test/java/com/example/blockhouse/BlockhouseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.blockhouse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlockhouseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
